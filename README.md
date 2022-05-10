253c2429294fa3a3303832940ada42002b2361d0
Daniel <mewregi@gmail.com>
2022-05-10 11:56:08 -0700

    can move mouse over squares to color them in, can press the button to create a grid with custom amount of squares per side

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 etch a sketch project 
 
-click on the grids to color them in
+move your mouse over the grids to color them in
+press the button to make a grid of custom size from 1 to 100 squares per side
--- javascript.js
+++ javascript.js
@@ -4,4 +4,29 @@ function createGrid() {
         element.classList.add("grid");
         document.getElementById("container").appendChild(element);
     }
+    document.querySelectorAll(".grid").forEach(item => {
+        item.addEventListener("mouseenter", e => {
+            item.style.backgroundColor = "gray";
+        })
+    });
+}
+
+function createCustomGrid(n) {
+    if (n <= 100 && n > 0) {
+        document.querySelectorAll(".grid").forEach(item => {
+            item.remove();
+        });
+        for (let i = 0; i < n * n; i++) {
+            var element = document.createElement("div");
+            element.classList.add("grid");
+            document.getElementById("container").appendChild(element);
+        }
+        document.getElementById("container").style.gridTemplateColumns = "repeat(" + n + ", 1fr)";
+        document.getElementById("container").style.gridTemplateRows = "repeat(" + n + ", 1fr)";
+        document.querySelectorAll(".grid").forEach(item => {
+            item.addEventListener("mouseenter", e => {
+                item.style.backgroundColor = "gray";
+            })
+        });
+    }
 }
